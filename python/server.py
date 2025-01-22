4ba6148ed5756a7c37fd17c255f56e398e727b94
fred ver <fred.ver06@gmail.com>
2025-01-22 17:26:45 +0100

    Finished backend logic for adding dipendenti

--- python/server.py
+++ python/server.py
@@ -8,26 +8,27 @@ import fredbconn
 import fredauth
 
 #TODO metti che il testo delle note se è troppo fa i puntini (su css)
+#TODO aggiungi bottone logout
+#TODO fare una interfaccia grafica e un form per aggiungere i dipendenti
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.secret_key = passwords.app_secret_key
 
 class Dipendente:
 
-    def __init__(self, nome: str, cognome: str, ditta_id: int, is_badge_already_emesso: int, autorizzato: int):
+    def __init__(self, nome: str, cognome: str, ditta_name: str, is_badge_already_emesso: int, autorizzato: int):
         self.nome = nome
         self.cognome = cognome
-        self.ditta_id = ditta_id
+        self.ditta_name = ditta_name
         self.is_badge_already_emesso = is_badge_already_emesso
         self.autorizzato = autorizzato
     
-
     @classmethod
     def from_form(cls):
         """Transforms the request form into a Dipendente object
 
         Raises:
-            BadRequest: either "nome", "cognome", "ditta_id", "is_badge_already_emesso" based on what field is missing
+            BadRequest: either "nome", "cognome", "ditta_name" based on what field is missing
 
         Returns:
             Dipendente: an object of type Dipendente
@@ -38,8 +39,8 @@ class Dipendente:
         cognome = request.form.get("cognome")
         if (cognome == None) or (cognome == ""): raise BadRequest("cognome")
 
-        ditta_id = request.form.get("ditta_id")
-        if ditta_id is None: raise BadRequest("ditta_id")
+        ditta_name = request.form.get("ditta_name")
+        if ditta_name is None: raise BadRequest("ditta_name")
 
         is_badge_already_emesso = request.form.get("is_badge_already_emesso")
         if is_badge_already_emesso is None: is_badge_already_emesso = 0
@@ -49,19 +50,33 @@ class Dipendente:
         if autorizzato is None: autorizzato = 0
         if autorizzato == "yes": autorizzato = 1
 
-        return cls(nome, cognome, ditta_id, is_badge_already_emesso, autorizzato)
+        return cls(nome, cognome, ditta_name, is_badge_already_emesso, autorizzato)
     
     
     def get_fields(self):
-        return self.nome, self.cognome, self.ditta_id, self.is_badge_already_emesso, self.autorizzato
+        return self.nome, self.cognome, self.ditta_name, self.is_badge_already_emesso, self.autorizzato
     
     @fredbconn.connected_to_database
     def add_to_db(cursor, self):
+
         fields = self.get_fields()
+
+        cursor.execute("""
+        SELECT id
+        FROM ditte
+        WHERE nome = %s
+        """, (fields[2],))
+
+        ditta_id = cursor.fetchone()
+
+        if ditta_id is None:
+            flash("La ditta selezionata è stata rimossa", "error")
+            return redirect("/aggiungi-dipendenti")
+
         cursor.execute("""
         INSERT INTO dipendenti(nome, cognome, ditta_id, is_badge_already_emesso, autorizzato)
         VALUES (%s, %s, %s, %s, %s)
-        """, (fields[0], fields[1], fields[2], fields[3], fields[4]))
+        """, (fields[0], fields[1], ditta_id, fields[3], fields[4]))
 
 
 @app.route("/")
@@ -98,17 +113,25 @@ def aggiungi_dipendenti():
     # Will never return None because of fredauth.authorized
     if fetch_is_admin()[0] == 0:
         flash("Il suo account non dispone delle autorizzazioni necessarie per aggiungere o modificare dipendenti",
-               "autenticazione-fallita")
+                "error")
         return redirect("/dipendenti")
     
     if request.method == "GET":
         return render_template("aggiungi-dipendenti.html")
-    
+    #TODO testa ognuno di questi messaggi di errore
     if request.method == "POST":
         try:
             dipendente = Dipendente.from_form()
         except BadRequest as e:
-            flash(f"Non ha inserito il campo {e}")
+            match str(e):
+                case "nome":
+                    field_error = "nome dipendente"
+                case "cognome":
+                    field_error = "cognome dipendente"
+                case "ditta_name":
+                    field_error = "nome ditta"
+
+            flash(f"Non ha inserito il campo {field_error}", "error")
             return redirect("/aggiungi-dipendenti")
         else:
             dipendente.add_to_db()
@@ -129,15 +152,15 @@ def login():
         fetched = fetch_info()
 
         if not fetched:
-            flash("L'utente non è registrato", "autenticazione-fallita")
+            flash("L'utente non è registrato", "error")
             return redirect("/login")
         
         if not password == fetched[0]:
-            flash("La password è sbagliata", "autenticazione-fallita")
+            flash("La password è sbagliata", "error")
             return redirect("/login")
 
         if not fetched[1] == 1:
-            flash("Il suo account è stato disabilitato.", "autenticazione-fallita")
+            flash("Il suo account è stato disabilitato.", "error")
             return redirect("/login")
 
         session["user"] = username
