fec0de03e5b714729d3e66ae40bcc8e58ffcdc8e
fred ver <fred.ver06@gmail.com>
2025-03-24 21:10:02 +0100

    Added feature: script to send reports via email

--- python/server.py
+++ python/server.py
@@ -1071,5 +1071,5 @@ if __name__ == "__main__":
 
     crash_logger = CrashLogger()
 
-    # serve(app, host='0.0.0.0', port=16000)
-    app.run(host="127.0.0.1", port="5000", debug=True)
+    serve(app, host='0.0.0.0', port=16000)
+    # app.run(host="127.0.0.1", port="5000", debug=True)
